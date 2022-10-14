be5254fce4383defdd4d564b6b93df441ab8e46d
Shenba Maruthamuthu <shenbamaruthu@gmail.com>
2022-10-14 18:53:55 -0400

    Updates

--- NYCTaxiTrips.py
+++ NYCTaxiTrips.py
@@ -1,19 +1,22 @@
 import csv
 import datetime
+from math import radians, cos, sin, asin, sqrt
 
 
 def ReadCsv():
     fl=open(csvname,'r')
     reader=csv.reader(fl)
     n = 0
-  #  for row in reader:
-   #     if n < 6:
-    #       print (row)
-     #   n+=1
-    return(reader)
+    for row in reader:
+       if n < 6:
+           print (row)
+       n+=1
+    #return(reader)
 
 
-def question1(data):
+def question1():
+    fl = open(csvname, 'r')
+    data = csv.reader(fl)
     '''Question 1 -
         What datetime range does your data cover?  How many rows are there total '''
     len=0
@@ -41,7 +44,9 @@ def question1(data):
     print(f"Time Range {min_pickup} between {max_dropoff} ")
 
 
-def question5(data):
+def question5():
+    fl = open(csvname, 'r')
+    data = csv.reader(fl)
     n =0
     min_plong = 0
     max_plog = 0
@@ -87,17 +92,109 @@ def question5(data):
     print(f"Min and Max of dropoff_Longitude :{min_dlong} and {max_dlong} ")
     print(f"Min and Max of dropoff_Longitude :{min_dlat} and {max_dlat} ")
 
-def question6(data):
-    pass
+def question6():
+    fl = open(csvname, 'r')
+    data = csv.reader(fl)
+    outputlist=[]
+    n = 0
+    for row in data:
+        if n >0:
+            try:
+                plong = float(row[10])
+                plat = float(row[11])
+                dlong = float(row[12])
+                dlat = float(row[13])
+            except Exception as e:
+                print (e)
+            plong, plat, dlong, dlat = map(radians, [plong, plat, dlong, dlat])
+            if plong < 99 and plong > -99:
+                dlon = dlong - plong
+                dlat = dlat - plat
+                a = sin(dlat / 2) ** 2 + cos(plat) * cos(dlat) * sin(dlon / 2) ** 2
+                print(a)
+                c = 2 * asin(sqrt(a))
+                r = 6371
+                out=c*r
+                outputlist.append[out]
+        n=+1
+    print (outputlist)
+
+def question7():
+
+    distinctdict={}
+    header = ['vendor_id','rate_code','store_and_fwd_flag','passenger_count']
+
+    i=0
+    for H in header:
+        print(H)
+        if H == 'vendor_id':
+            index = 2
+        elif H == 'rate_code':
+            index = 3
+        elif H == 'store_and_fwd_flag':
+            index = 4
+        elif H == 'passenger_count':
+            index = 7
+
+        fl = open(csvname, 'r')
+        data = csv.reader(fl)
+        distinctValues = []
+        n = 0
+        for row in data:
+            if n > 0:
+                #print(index)
+                H_value = row[index]
+                if H_value not in distinctValues:
+                    distinctValues.append(H_value)
+            n+=1
+        distinctdict[H] = distinctValues
+
+    print(distinctdict)
+
+def question8():
+    header = ['passenger_count', 'trip_time_in_secs', 'trip_distance']
+    #, 'trip_time_in_secs', 'trip_distance'
+    for H in header:
+        print (H)
+        if H == 'passenger_count':
+            index = 7
+        elif H == 'trip_time_in_secs':
+            index = 8
+        elif H == 'trip_distance':
+            index = 9
+        fl = open(csvname, 'r')
+        data = csv.reader(fl)
+        n = 0
+        min_h=0
+        max_h=0
+        dict={}
+        distinctValues={}
+        for row in data:
+            if n > 0:
+                H_value = float(row[index])
+                if min_h == 0 and min_h > H_value:
+                    min_h = H_value
+                    #dict['min']=min_h
+                if max_h == 0 and max_h < H_value:
+                    max_h = H_value
+                    #dict['max'] = max_h
+            n+=1
+            dict['min'] = min_h
+            dict['max'] = max_h
+        distinctValues[H] = dict
+    print (distinctValues)
+
 
 
 csvname="trip_data_1.csv"
 if __name__ == '__main__':
     #readcsv()
     starttime=datetime.datetime.now()
-    Dataset=ReadCsv()
-    question1(Dataset)
-    question5(Dataset)
-    question6(Dataset)
+    #ReadCsv()
+    #question1()
+    #question5()
+    ##question6()
+    ##question7()
+    question8()
     endtime=datetime.datetime.now()-starttime
     print(f"{endtime} : Time Taken")
